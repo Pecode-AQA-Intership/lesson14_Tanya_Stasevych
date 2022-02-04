45ac883f905661dbfe8945735d5d8b9436661ee1
Stasevych <tanyastasevich5@gmail.com>
2022-02-04 20:43:05 +0200

    added (wait) to each test

--- cypress/integration/2-advanced-examples/STASEVYCH_first.spec.js
+++ cypress/integration/2-advanced-examples/STASEVYCH_first.spec.js
@@ -7,18 +7,23 @@ describe('Fill in fields with data', () => {
     cy.visit('https://demoqa.com/text-box');
   });
   it('Full Name field', () => {
+    cy.wait(2000)
     cy.get('#userName').type(randomName).should('have.value', randomName);
   });
   it('Email field', () => {
+    cy.wait(2000)
     cy.get('#userEmail').type(randomEmail).should('have.value', randomEmail);
   });
   it('Current Address', () => {
+    cy.wait(2000)
     cy.get('#currentAddress').type(randomAddress).should('have.value',randomAddress);
   });
   it('Permanent Address', () => {
+    cy.wait(2000)
     cy.get('#permanentAddress').type(randomAddress).should('have.value', randomAddress);
   });
   it('Click Submit button', () => {
+    cy.wait(2000)
     cy.get('#submit').click();
   });
 });
